bc56a18329b717722f39af15b3954bad1201c90f
Ignacio Santillán <nachosantillann@gmail.com>
2021-11-08 12:04:33 -0300

    realizandoCambiosEstructurales

--- JS/modal.js
+++ JS/modal.js
@@ -1,4 +1,17 @@
-const modalAbrir = document.getElementById('modal-abrir')
+/* <div class="modal-container">
+<div class="modal-abrir modalcito">
+    <div><img class="imagenModal1" src="./IMAGENES/kita.png" alt=""></div>
+    <div>VENI A DISFRUTAR A KITA</div>
+    <div class="carritoCompras"><button class="buttonImagenModalcito" id="carrito-compras"><img src="./IMAGENES/carrito.jpg" alt=""></button></div>
+    <div><button class="buttonModalcito" id="modal-cerrar">CERRAR</button></div>
+</div>
+</div> 
+
+___________________________esto de arriba iria en el html_____________________________ */
+
+
+
+const modalAbrir = document.getElementById('modal-abrir ')
 const modalCerrar = document.getElementById('modal-cerrar')
 const modalContainer = document.getElementsByClassName('modal-container')[0]
 const modal = document.getElementsByClassName('modal')[0]
@@ -13,9 +26,7 @@ modalCerrar.addEventListener('click', () => {
     // modalContainer.classList.toggle('modal-active')
 })
 
-modalContainer.addEventListener('click', () => {
-    modalContainer.classList.remove('modal-active')
-})
+
 
 modal.addEventListener('click', (e) =>{
     e.stopPropagation() 
