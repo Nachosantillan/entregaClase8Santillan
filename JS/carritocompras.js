25285900fd27f279294e940fd434ed2f59e1133a
Ignacio Santillán <nachosantillann@gmail.com>
2021-11-14 22:53:37 -0300

    arreglando

--- JS/carritocompras.js
+++
@@ -1,7 +0,0 @@
-const carritoCompras = document.getElementById('carrito-compras')
-
-carritoCompras.addEventListener("click", agregarCarritoCompras)
-
-function agregarCarritoCompras(){
-    console.log("agrego esta hamburguesa")
-}
--- JS/hamburguesas.js
+++
@@ -1,6 +0,0 @@
-const hamburguesas = document.getElementById('btnSelectHamburguesas')
-
-hamburguesas.addEventListener('click', () => {
-    console.log('clickeaste el boton')
-
-})
