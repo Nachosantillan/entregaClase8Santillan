00af9d48108c0cfc4cfa4e912009718efdd6176e
Ignacio Santillán <nachosantillann@gmail.com>
2021-12-01 23:48:11 -0300

    agregando y cambiando todo

---
+++ JS/indexPrincipal.js
@@ -0,0 +1,536 @@
+
+// arrayHamburguesas_________________________________________________________
+
+const arrayHamburguesas=[
+    {img: "./IMAGENES/CHAMPIONSLEAGUE.PNG", id:0,  nombre: "CHAMPIONS LEAGUE", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/CHE_DARLING.PNG", id:1,  nombre: "CHE DARLING", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/DONALD.PNG", id:2,  nombre: "DONALD", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/KITACLUB.PNG", id:3,  nombre: "KITA CLUB", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/RICKY_FORT.PNG", id:4,  nombre: "RICKY FORT", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/KRYPTONITA_C_B.PNG", id:5,  nombre: "KRYPTONIYA CON C&B", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/KRYPTONITA_PROVO.PNG", id:6,  nombre: "KRYPTONITA CON PROVO", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/LV.PNG", id:7,  nombre: "LAS VEGAS", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/LV_RELLENA.PNG", id:8,  nombre: "LAS VEGAS RELLENA", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/RED_HOT.PNG", id:9,  nombre: "RED HOT", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/TOM_CHERRY.PNG", id:10,  nombre: "TOM & CHERRY", precioSimple: 600, precioDoble: 690, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/VEGGIE.PNG", id:11,  nombre: "CONVERTÍ EN VEGGIE", precioSimple: 650, precioDoble: 740, botonComprar: "¡LA QUIERO!"},
+
+]
+
+const contenedorHamburguesas = document.getElementById("productos")
+
+arrayHamburguesas.forEach((producto)=>{
+    
+    const div = document.createElement("div")
+    div.className = "card-body styleCard mb-2 text-center"
+    div.style = "max-width: 240px; max-height: 340px"
+
+        
+    div.innerHTML = `
+    
+    
+    <div>
+                        <img src=${producto.img} class="card-img-top" style="border-color: white" alt="imagenHamburguesa">
+                        <h5 class="card-title styleTitleCard">${producto.nombre}</h5>
+                        <p class="card-text styleSubtitleCard">Precio: $ ${producto.precioSimple}</p>
+                        <div id="buttonCarrito${producto.id}"><button id="modal-abrir laQuieroHamburguesa" value=${producto.nombre} class="buttonModalcito">LA QUIERO</button></div>
+                            
+                            
+                    </div>
+                `
+
+                
+contenedorHamburguesas.appendChild(div)
+
+})
+
+// arrayPicadas_________________________________________________________
+
+
+const arrayPicadas=[
+    {img: "./IMAGENES/papasFritas.PNG",  nombre: "PAPAS CON CHEDDAR, BACON Y VERDEO", precioSimple: 470, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/papasFritas2.PNG",  nombre: "PAPAS CON CEBOLLA A LA CREMA", precioSimple: 470, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/arosDeCebolla.PNG",  nombre: "AROS DE CEBOLLA CON BARBACOA", precioSimple: 450, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/nuggetsYPapas.PNG",  nombre: "NUGGETS CON PAPAS FRITAS", precioSimple: 550, botonComprar: "¡LA QUIERO!"},
+
+]
+
+const contenedorPicadas = document.getElementById("productosPicadas")
+
+arrayPicadas.forEach((producto)=>{
+    
+    const div = document.createElement("div")
+    div.className = "card-body styleCard mb-2 text-center"
+    div.style = "max-width: 240px; max-height: 340px"
+
+        
+    div.innerHTML = `
+    
+    
+    <div>
+                        <img src=${producto.img} class="card-img-top" style="border-color: white" alt="imagenHamburguesa">
+                        <h5 class="card-title styleTitleCard">${producto.nombre}</h5>
+                        <p class="card-text styleSubtitleCard">Precio: $ ${producto.precioSimple}</p>
+                        <div id="buttonCarrito${producto.id}"><button id="modal-abrir laQuieroHamburguesa" value=${producto.nombre} class="buttonModalcito">LA QUIERO</button></div>
+                            
+                            
+                    </div>
+                `
+
+                
+contenedorPicadas.appendChild(div)
+
+})
+
+
+// arrayPizzas_________________________________________________________
+
+
+
+const arrayPizzas=[
+    {img: "./IMAGENES/muzza.PNG",  nombre: "MUZZA", precioSimple: 450, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/napolitana.PNG",  nombre: "NAPOLITANA", precioSimple: 480, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/rucula.PNG",  nombre: "RUCULA Y CRUDO", precioSimple: 480, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/cebollaycrema.PNG",  nombre: "CEBOLLA A LA CREMA", precioSimple: 480, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/RUCULA.PNG",  nombre: "CHEDDAR", precioSimple: 480, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/4quesos.PNG",  nombre: "CUATRO QUESOS", precioSimple: 480, botonComprar: "¡LA QUIERO!"},
+
+]
+
+const contenedorPizzas = document.getElementById("productosPizzas")
+
+arrayPizzas.forEach((producto)=>{
+    
+    const div = document.createElement("div")
+    div.className = "card-body styleCard mb-2 text-center"
+    div.style = "max-width: 240px; max-height: 340px"
+
+        
+    div.innerHTML = `
+    
+    
+    <div>
+                        <img src=${producto.img} class="card-img-top" style="border-color: white" alt="imagenHamburguesa">
+                        <h5 class="card-title styleTitleCard">${producto.nombre}</h5>
+                        <p class="card-text styleSubtitleCard">Precio: $ ${producto.precioSimple}</p>
+                        <div id="buttonCarrito${producto.id}"><button id="modal-abrir laQuieroHamburguesa" value=${producto.nombre} class="buttonModalcito">LA QUIERO</button></div>
+                            
+                            
+                    </div>
+                `
+
+                
+contenedorPizzas.appendChild(div)
+
+})
+
+
+// arrayWraps_________________________________________________________
+
+
+
+const arrayWraps=[
+    {img: "./IMAGENES/wrapParmesano.PNG",  nombre: "PARMESANO", precioSimple: 440, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/wrapMediterraneo.PNG",  nombre: "MEDITERRANEO", precioSimple: 440, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/wrapGriego.PNG",  nombre: "GRIEGO", precioSimple: 440, botonComprar: "¡LA QUIERO!"},
+    {img: "./IMAGENES/wrapJamonYMuza.PNG",  nombre: "JAMON Y MUZZA", precioSimple: 440, botonComprar: "¡LA QUIERO!"},
+
+]
+
+const contenedorWraps = document.getElementById("productosWraps")
+
+arrayWraps.forEach((producto)=>{
+    
+    const div = document.createElement("div")
+    div.className = "card-body styleCard mb-2 text-center"
+    div.style = "max-width: 240px; max-height: 340px"
+
+        
+    div.innerHTML = `
+    
+    
+    <div>
+                        <img src=${producto.img} class="card-img-top" style="border-color: white" alt="imagenHamburguesa">
+                        <h5 class="card-title styleTitleCard">${producto.nombre}</h5>
+                        <p class="card-text styleSubtitleCard">Precio: $ ${producto.precioSimple}</p>
+                        <div id="buttonCarrito${producto.id}"><button id="modal-abrir laQuieroHamburguesa" value=${producto.nombre} class="buttonModalcito">LA QUIERO</button></div>
+                            
+                            
+                    </div>
+                `
+
+                
+contenedorWraps.appendChild(div)
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// MODAL CARRITO
+
+const contenedorModal = document.getElementsByClassName('modal-contenedor')[0]
+const botonAbrir = document.getElementById('boton-carrito')
+const botonCerrar = document.getElementById('carritoCerrar')
+const modalCarrito = document.getElementsByClassName('modal-carrito')[0]
+const productosTablaCarrito = document.getElementById('productosTablaCarrito')
+const totalPedido = document.getElementById('totalPedido')
+const confirmarPedido = document.getElementById('confirmarPedido')
+const vaciarCarrito = document.getElementById('vaciarCarrito')
+
+botonAbrir.addEventListener('click', ()=>{
+    contenedorModal.classList.toggle('modal-active')
+})
+botonCerrar.addEventListener('click', ()=>{
+    contenedorModal.classList.toggle('modal-active')
+})
+contenedorModal.addEventListener('click', ()=>{
+    botonCerrar.click()
+})
+modalCarrito.addEventListener('click', (event)=>{
+    event.stopPropagation()
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//FOR______________________________________________________________________________________________________________________________________
+
+let itemsCarrito2 = 0
+
+if(localStorage.length == 0){
+    itemsCarrito2 = 0
+}else{
+    for(let i = 0 ; i<localStorage.length ; i++){
+        let key = localStorage.key(i) 
+        let value = localStorage.getItem(key)
+        itemsCarrito2 = itemsCarrito2+parseInt(value)
+        
+}
+}
+
+
+for(let i=0 ; i<arrayProductos.length ; i++){
+    const hamburguesaCarrito = document.getElementById(`buttonCarrito${i}`)
+
+
+    hamburguesaCarrito.addEventListener('click', (event)=>{
+        let itemLocalStorage = localStorage.getItem(arrayProductos[i].nombre) 
+        if(itemLocalStorage) {
+            let newQuatity = parseInt(itemLocalStorage)+1
+            localStorage.setItem(arrayProductos[i].nombre, newQuatity)
+        } else {
+            localStorage.setItem(arrayProductos[i].nombre, 1)
+        }
+        itemsCarrito2=itemsCarrito2+1
+        itemsCarrito.innerHTML=`
+            <span>
+                ${itemsCarrito2}
+            </span>
+        `
+
+        let str = ``
+        for(let i = 0 ; i<localStorage.length ; i++){
+            let key = localStorage.key(i) 
+            let value = localStorage.getItem(key)
+            let precio = 0
+            
+            for(let j = 0 ; j<arrayProductos.length ; j++){
+                if(arrayProductos[j].nombre === key) {
+                    precio = arrayProductos[j].precioSimple  
+                } 
+            }
+    
+            let precioTotal = precio*parseInt(value)
+            subtotalBurger = subtotalBurger + precioTotal
+    
+            str = str.concat(`
+                    <tr>
+                        <td>${key}</td>
+                            <td class="text-center">
+                                <button class='botonRestarProducto${key} style='background-color: transparent; color: white'>
+                                    <i class='bx bx-minus-circle'></i>
+                                </button> 
+                                    ${value} 
+                                <button class='botonSumarProducto${key} style='background-color: transparent; color: white'>
+                                    <i class='bx bx-plus-circle'></i>
+                                </button>
+                            </td>
+                        <td class="text-center">$ ${precio}</td>
+                        <td class="text-center">$ ${precioTotal}</td>
+                    </tr>
+                `
+            )
+
+            productosTablaCarrito.innerHTML=`
+                ${str}
+            `
+
+            totalPedido.innerHTML=`
+                <h6>SUBTOTAL: $${subtotalBurger}</h6>
+            `
+        }
+    })
+}
+
+
+localStorage.getItem('hamburguesa')
+
+const itemsCarrito = document.getElementById('itemsCarrito')
+let subtotalBurger = 0
+
+if(itemsCarrito2==0){
+    itemsCarrito.innerHTML=`
+    <span>
+        El carrito está vacío
+    </span>
+`
+}else{
+    itemsCarrito.innerHTML=`
+    <span>
+        ${itemsCarrito2}
+    </span>
+`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//MODAL CARRITO SUMANDO PRODUCTOS
+
+if(localStorage.length == 0){
+    productosTablaCarrito.innerHTML=`
+    <span>
+        El carrito está vacío
+    </span>
+`
+    
+}else{
+    let str = ``
+    for(let i = 0 ; i<localStorage.length ; i++){
+        let key = localStorage.key(i) 
+        let value = localStorage.getItem(key)
+        let precio = 0
+        
+        for(let j = 0 ; j<arrayProductos.length ; j++){
+            if(arrayProductos[j].nombre === key) {
+                precio = arrayProductos[j].precioSimple  
+            } 
+        }
+
+        let precioTotal = precio*parseInt(value)
+        subtotalBurger = subtotalBurger + precioTotal
+
+        str = str.concat(`
+                <tr>
+                    <td>${key}</td>
+                        <td class="text-center">
+                            <button class='botonRestarProducto${key} style='background-color: transparent; color: white'>
+                                <i class='bx bx-minus-circle'></i>
+                            </button> 
+                                ${value} 
+                            <button class='botonSumarProducto${key} style='background-color: transparent; color: white'>
+                                <i class='bx bx-plus-circle'></i>
+                            </button>
+                        </td>
+                    <td class="text-center">$ ${precio}</td>
+                    <td class="text-center">$ ${precioTotal}</td>
+                </tr>
+            `
+        )
+    }
+
+    productosTablaCarrito.innerHTML=`
+        ${str}
+        
+        `
+        totalPedido.innerHTML=`
+                <h6>SUBTOTAL: $${subtotalBurger}</h6>
+                
+            `
+}
+vaciarCarrito.addEventListener('click', () =>{
+    localStorage.clear()
+
+    productosTablaCarrito.innerHTML=`
+        <span>
+            El carrito está vacío
+        </span>
+    `
+
+    itemsCarrito.innerHTML=`
+        <span>
+            El carrito está vacío
+        </span>
+    `
+
+    totalPedido.innerHTML=`
+        <h6>SUBTOTAL: NO HAS SELECCIONADO NINGÚN ITEM</h6>
+    `
+
+    itemsCarrito2=0
+    subtotalBurger=0
+})
+var actualizarCantidades=function(){
+    console.log("llegue a la fn")
+    let str = ``
+    for(let i = 0 ; i<localStorage.length ; i++){
+        let key = localStorage.key(i) 
+        let value = localStorage.getItem(key)
+        let precio = 0
+        
+        for(let j = 0 ; j<arrayProductos.length ; j++){
+            if(arrayProductos[j].nombre === key) {
+                precio = arrayProductos[j].precioSimple  
+            } 
+        }
+
+        let precioTotal = precio*parseInt(value)
+        subtotalBurger = subtotalBurger + precioTotal
+
+        str = str.concat(`
+                <tr>
+                    <td>${key}</td>
+                        <td class="text-center">
+                            <button class='botonRestarProducto${key} style='background-color: transparent; color: white'>
+                                <i class='bx bx-minus-circle'></i>
+                            </button> 
+                                ${value} 
+                            <button class='botonSumarProducto${key} style='background-color: transparent; color: white'>
+                                <i class='bx bx-plus-circle'></i>
+                            </button>
+                        </td>
+                    <td class="text-center">$ ${precio}</td>
+                    <td class="text-center">$ ${precioTotal}</td>
+                </tr>
+            `
+        )
+    }
+
+    productosTablaCarrito.innerHTML=`
+        ${str}
+        
+    `
+    totalPedido.innerHTML=`
+        <h6>SUBTOTAL: $${subtotalBurger}</h6>
+    `
+}
+
+for(let i=0; i<localStorage.length ; i++){
+    let key = localStorage.key(i) 
+    const botonRestar = document.getElementsByClassName(`botonRestarProducto${key}`)[0]
+    const botonSumar = document.getElementsByClassName(`botonSumarProducto${key}`)[0]
+
+    botonRestar.addEventListener('click', () =>{
+        let value = localStorage.getItem(key)
+        value=parseInt(value)-1
+        if(value >= 0) {
+            localStorage.setItem(key, value)
+        }
+        console.log("HOLA LLEGUE")
+        actualizarCantidades()
+    })
+
+    botonSumar.addEventListener('click', () =>{
+        let value = localStorage.getItem(key)
+        value=parseInt(value)+1
+        localStorage.setItem(key, value)
+        // actualizarCantidades()
+    })
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
